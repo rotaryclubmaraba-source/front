97b0a928b3dc8a0a34dd17a47b9e7e325d8f048a
Elson Jr <jhule.vaz@gmail.com>
2026-03-13 06:30:43 -0300

    atualização oficial do Vlibras

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from '@/lib/AuthContext';
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Rotary Club de Marabá',
@@ -38,22 +37,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           />
         </AuthProvider>
 
-        {/* VLibras */}
-        <div {...{ vw: 'true' }}>
-          <div {...{ 'vw-access-button': 'true' }} className="active"></div>
-          <div {...{ 'vw-plugin-wrapper': 'true' }}>
-            <div className="vw-plugin-top-wrapper"></div>
-          </div>
-        </div>
-
-        <Script
-          id="vlibras"
-          src="https://vlibras.gov.br/app/vlibras-plugin.js"
-          strategy="afterInteractive"
-          onLoad={() => {
-            try {
-              new (window as any).VLibras.Widget('https://vlibras.gov.br/app');
-            } catch(e) {}
+        {/* VLibras - método oficial */}
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <div vw="true" class="enabled">
+                <div vw-access-button="true" class="active"></div>
+                <div vw-plugin-wrapper="true">
+                  <div class="vw-plugin-top-wrapper"></div>
+                </div>
+              </div>
+              <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+              <script>new window.VLibras.Widget('https://vlibras.gov.br/app');</script>
+            `
           }}
         />
       </body>
